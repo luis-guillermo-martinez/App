b6a30a461a3f453b2ff52857a9dfbb9695f72030
Luis Martinez <luismartinezborgogno@gmail.com>
2022-04-07 23:42:49 -0300

    Pasado a Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    AptitudesComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule
